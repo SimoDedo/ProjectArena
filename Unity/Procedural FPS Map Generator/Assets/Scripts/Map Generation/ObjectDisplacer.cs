8a32053ee13ac705676bc8127f466479386868a0
Marco Ballabio <marco.ballabio@outlook.it>
2017-09-30 16:17:40 +0200

    Imported some models. Added the RotateTranslateByAxis script. Started working on the Oject Displacer.

---
+++ Unity/Procedural FPS Map Generator/Assets/Scripts/Map Generation/ObjectDisplacer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectDisplacer : MonoBehaviour {
+
+    // Custom objects that will be added to the map.
+    [SerializeField] private CustomObject[] customObjects;
+    // Object scale correction factor.
+    [SerializeField] private float sizeCorrection = 1f;
+
+    // Displace the custom objects inside the map.
+    public void DisplaceObjects(char[,] map, float height, float squareSize) {
+
+    }
+    
+    // Informations about an object. 
+    [Serializable]
+    private struct CustomObject {
+        // Character which defines the object.
+        public char objectChar;
+        // Number of objects to be put in the map.
+        public int numObjPerMap;
+    }
+
+}
