b05d1a921a41a6fd417b271c0264c64310ecdc86
Simone Bari <simone.bari@mail.polimi.it>
2022-01-09 10:07:08 +0100

    feat: try a new short distance wander algorithm

--- Unity/Project Arena/Assets/Scripts/AssemblyAI/Behaviours/Actions/SelectWanderDestination.cs
+++ Unity/Project Arena/Assets/Scripts/AssemblyAI/Behaviours/Actions/SelectWanderDestination.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using AssemblyAI.AI.Layer1.Actuator;
 using AssemblyAI.AI.Layer2;
 using AssemblyAI.Behaviours.Variables;
 using BehaviorDesigner.Runtime;
@@ -18,21 +20,24 @@ namespace AssemblyAI.Behaviours.Actions
         [SerializeField] private SharedFloat minWanderDistance = 20;
         [SerializeField] private SharedFloat maxWanderDistance = 20;
         [SerializeField] private SharedSelectedPathInfo pathChosen;
-        
+
         [SerializeField] private float nextUnrestrictedTravelTime;
 
         // TODO Cooldown should be only after 
         private const float UNRESTRICTED_TRAVEL_COOLDOWN = 20;
 
+        private AIMovementController movementController;
         private NavigationSystem navSystem;
 
         public override void OnAwake()
         {
-            navSystem = GetComponent<AIEntity>().NavigationSystem;
+            var entity = GetComponent<AIEntity>();
+            navSystem = entity.NavigationSystem;
+            movementController = entity.MovementController;
             // Do not immediately start with a long travel
             nextUnrestrictedTravelTime = Time.time + UNRESTRICTED_TRAVEL_COOLDOWN;
         }
-        
+
         public override TaskStatus OnUpdate()
         {
             var result = false;
@@ -41,11 +46,12 @@ namespace AssemblyAI.Behaviours.Actions
                 Debug.Log("Will now select long path!");
                 result = SelectLongWanderDestination();
             }
+
             if (result)
             {
                 Debug.Log("Long path valid destination found!");
                 // We got a valid destination! Apply cooldown
-                nextUnrestrictedTravelTime = Time.time + navSystem.EstimatePathDuration(pathChosen.Value) + 
+                nextUnrestrictedTravelTime = Time.time + navSystem.EstimatePathDuration(pathChosen.Value) +
                     UNRESTRICTED_TRAVEL_COOLDOWN;
             }
             else
@@ -80,24 +86,49 @@ namespace AssemblyAI.Behaviours.Actions
             return false;
         }
 
+        private const float LOOK_AHEAD = 10f;
+
         private bool SelectShortWanderDestination()
         {
-            var direction = transform.forward; //Or use velocity
+            var velocity = movementController.GetVelocity();
+            var direction = velocity == Vector3.zero ? transform.forward : velocity.normalized;
             for (var attempts = 0; attempts < maxRetries.Value; attempts++)
             {
-                bool validDestination;
-                direction += Random.insideUnitSphere * wanderRate.Value;
+                var newDisplacement = Random.insideUnitCircle * wanderRate.Value;
+                direction.x += newDisplacement.x;
+                direction.z += newDisplacement.y;
                 var destination = transform.position + direction.normalized *
                     Random.Range(minWanderDistance.Value, maxWanderDistance.Value);
-                validDestination = NavigationSystem.IsPointOnNavMesh(
-                    destination,
-                    NavMeshUtils.GetAgentIDFromName("Wanderer"),
-                    out destination
-                );
-                if (validDestination)
+
+                var path = navSystem.CalculatePath(destination);
+                if (path.IsComplete())
                 {
-                    var path = navSystem.CalculatePath(destination);
-                    if (path.IsComplete())
+                    // Check that the path doesn't lead in front of a wall...
+                    var corners = path.corners;
+                    var onGroundDestination = corners.Last();
+                    var validPointsFound = 0;
+
+                    var penultimatePoint = corners.Length < 2 ? transform.position : corners[corners.Length - 2];
+
+                    var forwardDirectionAtArrival = (onGroundDestination - penultimatePoint).normalized;
+
+                    Debug.DrawLine(penultimatePoint, onGroundDestination, Color.blue, 2f, false);
+
+                    for (var i = -4; i <= 4; i++)
+                    {
+                        var angle = i * 90f / 4;
+
+                        var currentLookDirection = Quaternion.AngleAxis(angle, Vector3.up) * forwardDirectionAtArrival;
+
+                        Debug.DrawRay(onGroundDestination, currentLookDirection * LOOK_AHEAD, Color.blue, 2f, false);
+
+                        if (!Physics.Raycast(onGroundDestination, currentLookDirection, LOOK_AHEAD))
+                        {
+                            validPointsFound++;
+                        }
+                    }
+
+                    if (validPointsFound > 3)
                     {
                         pathChosen.Value = path;
                         return true;
