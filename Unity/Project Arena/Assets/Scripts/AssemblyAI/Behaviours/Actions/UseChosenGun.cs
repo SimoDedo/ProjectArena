4f6f4bf38948d9e41606abe3b2f076f28e2ea294
Simone Bari <simone.bari@mail.polimi.it>
2022-01-26 16:06:46 +0100

    wip: reduce rocket launcher suicides

--- Unity/Project Arena/Assets/Scripts/AssemblyAI/Behaviours/Actions/UseChosenGun.cs
+++ Unity/Project Arena/Assets/Scripts/AssemblyAI/Behaviours/Actions/UseChosenGun.cs
@@ -14,8 +14,9 @@ namespace AssemblyAI.Behaviours.Actions
     [Serializable]
     public class UseChosenGun : Action
     {
-        [SerializeField] private int maxLookAheadFrames = 5;
-        [SerializeField] private float lookAheadTimeStep = 0.1f;
+        [SerializeField] private int lookBackFrames = -3;
+        [SerializeField] private int lookAheadFrames = 3;
+        [SerializeField] private float lookAheadTimeStep = 0.3f;
         [SerializeField] private SharedBool isGoingForCover;
         private AIEntity entity;
         private GunManager gunManager;
@@ -23,7 +24,6 @@ namespace AssemblyAI.Behaviours.Actions
         private PositionTracker enemyPositionTracker;
         private TargetKnowledgeBase targetKb;
         private AISightController sightController;
-        private AISightSensor sightSensor;
 
         private NormalDistribution distribution;
         private float previousReflexDelay;
@@ -37,7 +37,6 @@ namespace AssemblyAI.Behaviours.Actions
             gunManager = entity.GunManager;
             targetKb = entity.TargetKb;
             sightController = entity.SightController;
-            sightSensor = entity.SightSensor;
             enemy = entity.GetEnemy();
             enemyPositionTracker = enemy.GetComponent<PositionTracker>();
 
@@ -71,114 +70,135 @@ namespace AssemblyAI.Behaviours.Actions
 
             var lastSightedDelay = Time.time - lastSightedTime;
 
-            Vector3 position;
-            Vector3 velocity;
+            Vector3 enemyPosition;
+            Vector3 enemyVelocity;
 
             if (!float.IsNaN(lastSightedDelay) && lastSightedDelay > currentDelay)
             {
                 // We don't know the exact position of the enemy currentDelay ago, so just consider
                 // its last know position (with velocity zero, so that we won't correct the shooting position)
-                (position, velocity) = enemyPositionTracker.GetPositionAndVelocityFromDelay(lastSightedDelay);
+                (enemyPosition, enemyVelocity) = enemyPositionTracker.GetPositionAndVelocityFromDelay(lastSightedDelay);
             }
             else
             {
                 // We know the position of the enemy at currentDelay seconds ago, so use it directly.
-                (position, velocity) = enemyPositionTracker.GetPositionAndVelocityFromDelay(currentDelay);
+                (enemyPosition, enemyVelocity) = enemyPositionTracker.GetPositionAndVelocityFromDelay(currentDelay);
             }
 
-            float angle;
             var projectileSpeed = gunManager.GetCurrentGunProjectileSpeed();
             if (float.IsPositiveInfinity(projectileSpeed))
             {
-                // TODO Do not shoot if outside of gun range
-                angle = sightController.LookAtPoint(position);
-                if (lastSightedTime != Time.time && !gunManager.IsGunBlastWeapon(gunManager.CurrentGunIndex))
-                {
-                    // We don't see the enemy and we are not using a blast weapon, do not shoot.
-                    return TaskStatus.Running;
-                }
-                if (angle < 10 && gunManager.CanCurrentGunShoot() &&
-                    ShouldShootWeapon(sightController.GetHeadPosition(), position))
-                {
-                    gunManager.ShootCurrentGun();
-                }
+                AimRaycastWeapon(enemyPosition, lastSightedTime);
             }
             else
             {
-                // var distance = (transform.position - enemy.transform.position).magnitude;
-                // var estimatedTime = distance / projectileSpeed;
+                AimProjectileWeapon(enemyPosition, enemyVelocity, projectileSpeed);
+            }
 
-                var ourStartingPoint = sightController.GetHeadPosition();
-                var enemyStartPos = position;
-                // Default value: point on ground underneath enemy
-                var record = float.PositiveInfinity;
-                var chosenPoint = Vector3.zero;
-                var isGunBlast = gunManager.IsGunBlastWeapon(gunManager.CurrentGunIndex);
-                for (var i = 0; i <= maxLookAheadFrames; i++)
+            return TaskStatus.Running;
+        }
+
+        /**
+         * Tries to find the best position to aim at accounting for the weapon speed and the enemy estimated velocity.
+         * In case the weapon is a blast weapon, positions at the 
+         */
+        private void AimProjectileWeapon(Vector3 position, Vector3 velocity, float projectileSpeed)
+        {
+            var ourStartingPoint = sightController.GetHeadPosition();
+            var enemyStartPos = position;
+            var record = float.PositiveInfinity;
+            var chosenPoint = Vector3.zero;
+            var isGunBlast = gunManager.IsCurrentGunBlastWeapon();
+            for (var i = lookBackFrames; i <= lookAheadFrames; i++)
+            {
+                var newPos = enemyStartPos + velocity * (i * lookAheadTimeStep);
+
+                if (isGunBlast)
                 {
-                    var newPos = enemyStartPos + velocity * ((i-2) * lookAheadTimeStep);
-
-                    if (isGunBlast)
+                    // The weapon is a blast weapon. Aim at the enemy's feet.
+                    // TODO Check this raycast works. It fails sometimes... why?
+                    if (Physics.Raycast(newPos, Vector3.down * 5f, out var downRayHit))
                     {
-                        // The weapon is a blast weapon. Aim at the enemy's feet.
-                        // TODO Check this raycast works
-                        Physics.Raycast(newPos, Vector3.down, out var hit);
-                        newPos = hit.point;
-                    }
-
-                    if (!sightSensor.CanSeePosition(newPos)) continue;
-                    var distance = (ourStartingPoint - newPos).magnitude;
-                    var projectileTravelDistance = i * lookAheadTimeStep * projectileSpeed;
-                    if (distance - projectileTravelDistance < record)
-                    {
-                        record = distance - projectileTravelDistance;
-                        chosenPoint = newPos;
+                        newPos = downRayHit.point;
                     }
                 }
 
-                // Only shoot if we found a nice position, otherwise keep the shot for another time
-                if (float.IsPositiveInfinity(record)) return TaskStatus.Running;
-                
-                angle = sightController.LookAtPoint(chosenPoint);
-                if (angle < 40 && gunManager.CanCurrentGunShoot() &&
-                    ShouldShootWeapon(sightController.GetHeadPosition(), chosenPoint))
+                Debug.DrawLine(ourStartingPoint, newPos);
+                if (Physics.Linecast(ourStartingPoint, newPos, out var hit) && hit.point != newPos)
                 {
-                    gunManager.ShootCurrentGun();
+                    // Looks like there is an obstacle from out head to that position...
+                    continue;
+                }
+
+                var timeBeforeProjectileReachesNewPos = (ourStartingPoint - newPos).magnitude / projectileSpeed;
+                var timeError = Mathf.Abs(timeBeforeProjectileReachesNewPos - i * lookAheadTimeStep);
+
+                if (timeError < record)
+                {
+                    record = timeError;
+                    chosenPoint = newPos;
                 }
             }
 
+            // Only shoot if we found a nice position, otherwise keep the shot for another time
+            if (float.IsPositiveInfinity(record)) return;
 
-            return TaskStatus.Running;
+            var angle = sightController.LookAtPoint(chosenPoint);
+
+            if (angle < 10 && gunManager.CanCurrentGunShoot() && ShouldShootWeapon(chosenPoint, isGunBlast))
+            {
+                Debug.DrawRay(ourStartingPoint, sightController.GetHeadForward() * 100f, Color.blue, 2f);
+                Debug.Log("Shooting with angle " + angle);
+                gunManager.ShootCurrentGun();
+            }
+        }
+
+        private void AimRaycastWeapon(Vector3 position, float lastSightedTime)
+        {
+            // TODO Do not shoot if outside of gun range
+            var angle = sightController.LookAtPoint(position);
+            if (lastSightedTime != Time.time && !gunManager.IsGunBlastWeapon(gunManager.CurrentGunIndex))
+            {
+                // We don't see the enemy and we are not using a blast weapon, do not shoot.
+                return;
+            }
+
+            // TODO Understand angle... I should avoid shooting if I am using a blast weapon and the position in 
+            // front of me is too close!
+            if (angle < 10 && gunManager.CanCurrentGunShoot() &&
+                ShouldShootWeapon(position, gunManager.IsCurrentGunBlastWeapon()))
+            {
+                gunManager.ShootCurrentGun();
+            }
         }
 
         // I can shoot a gun if I do not detect any obstacle in the path from startingPos to position.
         // An obstacle is defined as something that would prevent the bullet from reaching at least 0.8
         // times the distance from startingPos to position.
-        private bool ShouldShootWeapon(Vector3 startingPos, Vector3 position)
+        private bool ShouldShootWeapon(Vector3 position, bool isBlastWeapon)
         {
+            var startingPos = sightController.GetHeadPosition();
             var distance = (startingPos - position).magnitude;
             if (gunManager.GetGunMaxRange(gunManager.CurrentGunIndex) < distance)
             {
                 // Outside of weapon range...
                 return false;
             }
-            // Try to cast a ray from our position to position. If it's too short, we have an obstacle
-            // in front of us...
-            var canShoot = false;
-            var raycastResult = Physics.Linecast(startingPos, position, out var hit);
-            if (raycastResult == false)
+
+            if (!isBlastWeapon)
             {
-                canShoot = true;
-            }
-            else
-            {
-                var distanceRatio = hit.distance / distance;
-                if (distanceRatio > 0.8f)
-                {
-                    canShoot = true;
-                }
+                return true;
             }
 
+            // Check that we do not hurt ourselves by shooting this weapon
+            var headForward = sightController.GetHeadForward();
+            // Try to cast a ray from our position to position. If it's too short, we have an obstacle
+            // in front of us...
+            bool canShoot;
+            var hitSomething = Physics.Raycast(startingPos, headForward, out var hit, distance);
+            // TODO Retrieve gun blast radius
+            canShoot = !hitSomething || hit.distance > 6.5f;
+            Debug.Log("Going to shoot rocket because " + hitSomething + " or " + hit.distance + " > 6.5f");
             return canShoot;
         }
     }
