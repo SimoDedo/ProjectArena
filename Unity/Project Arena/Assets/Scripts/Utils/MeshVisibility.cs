ffff192f5a0667c5ceb2fe9b57730478da3194f7
Simone Bari <simone.bari@mail.polimi.it>
2022-03-15 15:16:16 +0100

    feat: avoid disable renderers if server build

--- Unity/Project Arena/Assets/Scripts/Utils/MeshVisibility.cs
+++ Unity/Project Arena/Assets/Scripts/Utils/MeshVisibility.cs
@@ -7,12 +7,15 @@ namespace Utils
         // Hides/shows the mesh.
         public static void SetMeshVisible(Transform father, bool isVisible)
         {
+            // Avoid expensive operation in server build, since there is no rendering in any case
+            #if !UNITY_SERVER || UNITY_EDITOR
             foreach (Transform children in father)
             {
                 if (children.GetComponent<MeshRenderer>()) children.GetComponent<MeshRenderer>().enabled = isVisible;
 
                 SetMeshVisible(children, isVisible);
             }
+            #endif
         }
     }
 }
